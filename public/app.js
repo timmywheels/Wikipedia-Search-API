d9ae52fd3fa3ae6cc0f51023b2d4f759d5a4c56d
Christian Gracia <christiangracia.student@careerdevs.com>
2019-04-02 01:28:35 -0400

    fetch method

--- public/app.js
+++ public/app.js
@@ -13,15 +13,26 @@ app.use('public/assets', express.static('assets'));
 app.set("view engine", "ejs");
 
 app.get('/', function(req, res) {
-  res.render('index');
+    res.render('index');
 });
 
+
+
+
+function submit() {
+    const wikiQuery = document.getElementById('query').value;
+    var api = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search=" + wikiQuery + "&format=json&callback=?";
+
+
+    fetch(api).then(function(response) {
+            return response.json();
+        })
+        .then(function(myJson) {
+            console.log(JSON.stringify(myJson));
+        });
+}
 
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Now listening for requests");
 });
-
-
-
-
