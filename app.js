c79e62ac246ef41164144bf6b7a886166ef39aa8
Christian Gracia <christiangracia.student@careerdevs.com>
2019-04-03 20:01:08 -0400

    search functionality added

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 function submit() {
     const wikiQuery = document.getElementById('query').value;
-    var api = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search=" + wikiQuery + "&format=json&callback=?";
+    var cors = "https://cors-anywhere.herokuapp.com/";
+    var api = cors + "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + wikiQuery + "&limit=10&namespace=0&format=json&origin=*&utf8=&format=json";
 
-    fetch(api).then(response => response)
-        .then(data => {
+    fetch(api)
+        .then(function(response) {
+            return response.json();
+
+        })
+        .then(function(data) {
             console.log(data);
+            data[1].forEach(function(item) {
+                const listNode = document.createElement('li');
+                listNode.innerHTML = item;
+                document.getElementById("list").appendChild(listNode);
+            });
+            data[2].forEach(function(item) {
+                    var para = document.createElement('P');
+                    var t = document.createTextNode(item);
+                    para.appendChild(t);
+                    document.getElementsByTagName('li').appendChild(para);
+
+
+                });
         });
 }
